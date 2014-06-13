9e92b9aea1326ecd85cbc9afacb3d0c615064bed
sarah khalil <mkhalil.sarah@gmail.com>
2014-06-13 13:42:56 +0200

    --

--- src/SensioLabs/MeetingBundle/Controller/DefaultController.php
+++ src/SensioLabs/MeetingBundle/Controller/DefaultController.php
@@ -9,11 +9,20 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/")
      * @Template()
      */
     public function indexAction($name)
     {
         return array('name' => $name);
+    }
+
+    /**
+     * @Route("/meetings/one-to-one")
+     * @Template()
+     */
+    public function oneToOneAction()
+    {
+        return array();
     }
 }
